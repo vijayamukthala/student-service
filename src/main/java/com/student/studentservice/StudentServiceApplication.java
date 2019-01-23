442a508268ce3fbfee2b5492db17012737ed9fc9
vijaya.mukthala@wipro.com <vijaya.mukthala@wipro.com>
2019-01-23 20:42:46 +0530

    test

--- src/main/java/com/student/studentservice/StudentServiceApplication.java
+++ src/main/java/com/student/studentservice/StudentServiceApplication.java
@@ -3,6 +3,10 @@ package com.student.studentservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
+
+
 @SpringBootApplication
 public class StudentServiceApplication {
 
